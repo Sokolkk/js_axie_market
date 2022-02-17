7f0dbde6bd947988019b4e23ac73ff1b3e219f35
Sokolkk <47243051+Sokolkk@users.noreply.github.com>
2022-02-17 14:54:14 +0400

    Update axie_dahsboard.js
    add i for fix price

--- axie_dahsboard.js
+++ axie_dahsboard.js
@@ -8,12 +8,12 @@ let z7 = Number(getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]
 let z8 = Number(getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[8]/a/div/div[3]/div[1]/h6").innerHTML.slice(1, 4));
 let z9 = Number(getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[9]/a/div/div[3]/div[1]/h6").innerHTML.slice(1, 4));
 let z10 = Number(getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[1]/a/div/div[3]/div[1]/h6").innerHTML.slice(1, 4));
-
+let i = 33;
 
 //кликаем на элемент если сумма норм
 function check() {
 //	let z = Number(getElementByXpath("/html/body/div[1]/div[1]/div[4]/div[2]/div/div/div/div[2]/div[1]/a/div/div/div/div[4]/h6").innerHTML.slice(1, 3));
-  if (z1 < 34) {
+  if (z1 < i) {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[1]/a/div").click();
       //клик после задержки
       setTimeout(x, 1000);
@@ -23,7 +23,7 @@ function check() {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[1]/a/div").click();
       //клик после задержки
       setTimeout(x, 500);
-  } else if (z2 < 34) {
+  } else if (z2 < i) {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[2]/a/div").click();
       //клик после задержки
       setTimeout(x, 1000);
@@ -33,7 +33,7 @@ function check() {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[2]/a/div").click();
       //клик после задержки
       setTimeout(x, 500);
-  } else if (z3 < 34) {
+  } else if (z3 < i) {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[3]/a/div").click();
       //клик после задержки
       setTimeout(x, 1000);
@@ -43,7 +43,7 @@ function check() {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[3]/a/div").click();
       //клик после задержки
       setTimeout(x, 500);
-  } else if (z4 < 34) {
+  } else if (z4 < i) {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[4]/a/div").click();
       //клик после задержки
       setTimeout(x, 1000);
@@ -53,7 +53,7 @@ function check() {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[4]/a/div").click();
       //клик после задержки
       setTimeout(x, 500);
-  } else if (z5 < 34) {
+  } else if (z5 < i) {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[5]/a/div").click();
       //клик после задержки
       setTimeout(x, 1000);
@@ -63,7 +63,7 @@ function check() {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[5]/a/div").click();
       //клик после задержки
       setTimeout(x, 500);
-  } else if (z6 < 34) {
+  } else if (z6 < i) {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[6]/a/div").click();
       //клик после задержки
       setTimeout(x, 1000);
@@ -73,7 +73,7 @@ function check() {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[6]/a/div").click();
       //клик после задержки
       setTimeout(x, 500);
-  } else if (z7 < 34) {
+  } else if (z7 < i) {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[7]/a/div").click();
       //клик после задержки
       setTimeout(x, 1000);
@@ -83,7 +83,7 @@ function check() {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[7]/a/div").click();
       //клик после задержки
       setTimeout(x, 500);
-  } else if (z8 < 34) {
+  } else if (z8 < i) {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[8]/a/div").click();
       //клик после задержки
       setTimeout(x, 1000);
@@ -93,7 +93,7 @@ function check() {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[8]/a/div").click();
       //клик после задержки
       setTimeout(x, 500);
-  } else if (z9 < 34) {
+  } else if (z9 < i) {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[9]/a/div").click();
       //клик после задержки
       setTimeout(x, 1000);
@@ -103,7 +103,7 @@ function check() {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[9]/a/div").click();
       //клик после задержки
       setTimeout(x, 500);
-  } else if (z10 < 34) {
+  } else if (z10 < i) {
       getElementByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[1]/div[2]/div[10]/a/div").click();
       //клик после задержки
       setTimeout(x, 1000);
